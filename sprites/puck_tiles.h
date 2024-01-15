58178177b95a03c551a694f53ddda0308157962c
Paul Buser <buser.paul@gmail.com>
2024-01-15 11:42:12 -0500

    Add puck sprites

---
+++ sprites/puck_tiles.h
@@ -0,0 +1,13 @@
+/*
+
+Filename: puck_tiles.h
+
+Tile Header File
+*/
+
+#define puck_tiles_count 4
+#define puck_tiles_bytes 64
+
+/* Start of tile array */
+extern const unsigned char puck_p1[];
+extern const unsigned char puck_p2[];
