9a3e45ae4a485f38dd942a144eeeac9d14df4e36
Paul Buser <buser.paul@gmail.com>
2024-01-21 00:45:55 -0500

    Use sprites for showing selection

--- drawing.c
+++ drawing.c
@@ -1,9 +1,18 @@
 #include "drawing.h"
 
+#include <gb/gb.h>
+#include <gb/metasprites.h>
+
 #include "fourgo.h"
 #include "board.h"
 
-#include "gb/gb.h"
+const metasprite_t player1_puck_metasprite[] = {
+    {.dy=-8, .dx=-8, .dtile=0, .props=0},
+    {.dy=0, .dx=8, .dtile=1, .props=0},
+    {.dy=8, .dx=-8, .dtile=2, .props=0},
+    {.dy=0, .dx=8, .dtile=3, .props=0},
+	METASPR_TERM
+};
 
 void board_to_bkg(uint8_t* row, uint8_t* col)
 {
@@ -16,24 +25,21 @@ void init_drawing(void)
   set_bkg_data(0*puck_tiles_count, puck_tiles_count, puck_px);
   set_bkg_data(1*puck_tiles_count, puck_tiles_count, puck_p1);
   set_bkg_data(2*puck_tiles_count, puck_tiles_count, puck_p2);
+  set_sprite_data(0*puck_tiles_count, puck_tiles_count, puck_p1);
+  set_sprite_data(1*puck_tiles_count, puck_tiles_count, puck_p2);
   init_bkg(0);
   SHOW_BKG;
+  SHOW_SPRITES;
 }
 
 void draw_selection(uint8_t selected_column, uint8_t player)
 {
-  uint8_t row = 0, col, row_bkg, col_bkg, tile_index;
-  for (col = 0; col < BOARD_WIDTH; col++)
-  {
-    tile_index = 0;
-    if (col == selected_column)
-      tile_index = player * puck_map_tiles;
-    col_bkg = col;
-    row_bkg = row;
-    board_to_bkg(&row_bkg, &col_bkg);
-    row_bkg -= puck_map_height;
-    set_bkg_based_tiles(col_bkg, row_bkg, puck_map_width, puck_map_height, puck_map, tile_index);
-  }
+  uint8_t base_tile;
+  if (player == P1)
+    base_tile = 0*puck_tiles_count;
+  else
+    base_tile = 1*puck_tiles_count;
+  move_metasprite_ex(player1_puck_metasprite, base_tile, 0, 0, selected_column * 16 + 40, 32);
 }
 
 
