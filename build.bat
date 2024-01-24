e1bf786dd5b7909e1581d667cc3bcf50a14670c5
Paul Buser <buser.paul@gmail.com>
2024-01-24 17:41:05 -0500

    Refactor puck sprite and bkg data

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 	-I%GBDK_HOME%\hUGETracker-1.0.4-windows\hUGEDriver\include ^
 	-Wl-llib\hUGEDriver.lib ^
 	-o fourgo.gb ^
-	board.c drawing.c sound.c fourgo.c ^
-	sprites\puck_map.c sprites\puck_tiles.c sprites\winner_lines.c
+	sprites\puck.c ^
+	sprites\winner_lines.c ^
+	board.c drawing.c sound.c fourgo.c
---
+++ sprites/puck.h
@@ -0,0 +1,17 @@
+#ifndef FOURGO_PUCK_H_
+#define FOURGO_PUCK_H_
+
+#include <gb/metasprites.h>
+
+#define puck_tiles_count 4
+
+#define puck_map_width 2
+#define puck_map_height 2
+
+extern metasprite_t puck_metasprite[];
+extern char puck_map[];
+extern const unsigned char puck_p1[];
+extern const unsigned char puck_p2[];
+extern const unsigned char puck_px[];
+
+#endif
